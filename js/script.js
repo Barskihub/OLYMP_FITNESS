8538a9da632a878caac7c9b2caf0a9cea78072dc
Arthur Barski <barski.10877716@gmail.com>
2019-02-02 18:58:07 +0300

    Add mobile nav menu
    123

--- js/script.js
+++ js/script.js
@@ -21,3 +21,10 @@ $(document).ready(function () {
     });
 
 });
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
